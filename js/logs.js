915944c1ad4027e34300ddc9f2af5f741dc02acd
Andrei Lima <102513288+Andrei-Lima@users.noreply.github.com>
2022-04-29 16:38:22 -0300

    the remake
    the remake

---
+++ js/logs.js
@@ -0,0 +1,39 @@
+const logs = [
+    ["29/04/2022", "Inauguração", "Website pessoal para registrar alguns dos meus projetos e afazeres"],
+];
+
+for (let i = 0; i < logs.length; i++) {
+
+    new_log(logs[i][0], logs[i][1], logs[i][2]);
+
+}
+
+function new_log(date, title, desc) {
+
+    var box = document.createElement("div");
+    box.className = "box";
+
+    var d = document.createElement("div");
+    d.className = "date";
+    var dateTxt = document.createTextNode(date);
+    d.appendChild(dateTxt);
+
+    var ttl = document.createElement("div");
+    ttl.className = "title";
+    var ttlTxt = document.createElement("h2");
+    ttlTxt.innerHTML = title;
+    ttl.appendChild(ttlTxt);
+
+    var dsc = document.createElement("div");
+    dsc.className = "desc";
+    var dscTxt = document.createElement("p");
+    dscTxt.innerHTML = desc;
+    dsc.appendChild(dscTxt);
+
+    box.appendChild(d);
+    box.appendChild(ttl);
+    box.appendChild(dsc);
+
+    document.getElementById("links").appendChild(box);
+    
+}
